b6b7d5ac0436fd2c51b290ab854866f18e7620d4
Deanna Norman <deannaleenorman@msn.com>
2020-11-17 11:26:12 -0800

    middleware and routes setup

---
+++ server.js
@@ -0,0 +1,33 @@
+// require modules
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+    // router modules
+const indexRouter = require('./routes/index');
+//const hikesRouter = require('./routes/hikes');
+const { response } = require('express');
+
+// express app
+const app = express();
+
+// port value
+const port = 3000;
+
+// connect to DB
+require('./config/database');
+
+// configure settings
+app.set('view engine', 'ejs');
+
+// mount middleware
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use('/', indexRouter);
+app.use('/hikes', hikesRouter);
+
+// app to listen
+app.listen(port, function() {
+    console.log(`Express is listening on port: ${port}`);
+});
